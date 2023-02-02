7b822da84ad227adf4ab4f3a739ee1f7dcff956d
Chalo1996 <emusyoka759@gmail.com>
2023-02-02 14:00:01 +0300

    Return the floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""2-floor."""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of n.
+
+    Args:
+        n (float): value to return floor.
+
+    Returns:
+        int: floor of n.
+    """
+
+    return int(n)
