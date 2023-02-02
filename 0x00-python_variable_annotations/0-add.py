a93363269d68978859c94eb7203e8ebd79845c8d
Chalo1996 <emusyoka759@gmail.com>
2023-02-02 13:15:14 +0300

    sums two float values

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0-add."""
+
+
+def add(a: float, b: float) -> float:
+    """Return sum of two float values.
+
+      The Attributes:
+        a : float - first value.
+        b : float - second value.
+    """
+
+    return (a + b)
