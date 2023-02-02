7d42246e2830ec2b104431e34a87e1ce96b6bf34
Chalo1996 <emusyoka759@gmail.com>
2023-02-02 14:17:52 +0300

    Return string representation of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""3-to_str."""
+
+
+def to_str(n: float) -> str:
+    """
+    Return a string representation of n.
+
+    Args:
+        n (float): a float representation of n.
+
+    Returns:
+        str: str representation of n.
+    """
+
+    return repr(n)
