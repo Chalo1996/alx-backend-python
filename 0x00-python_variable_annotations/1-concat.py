4eb1ce8a5dcca59412d4f49d85a0f322d5038b8e
Chalo1996 <emusyoka759@gmail.com>
2023-02-02 13:41:46 +0300

    concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""1-concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenate two strings.
+
+    Args:
+        str1 (str): first string.
+        str2 (str): second string.
+
+    Returns:
+        str: str1 + str2.
+    """
+
+    return (str1 + str2)
