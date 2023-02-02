bf8c70e5ef951cee42d462bc7635aac2e2032256
Chalo1996 <emusyoka759@gmail.com>
2023-02-02 13:45:12 +0300

    sums two float values

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -8,6 +8,9 @@ def add(a: float, b: float) -> float:
       The Attributes:
         a : float - first value.
         b : float - second value.
+
+      Returns:
+        float: a + b.
     """
 
     return (a + b)
