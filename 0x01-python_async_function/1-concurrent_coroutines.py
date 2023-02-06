0f9dcb2262d8b93fb40304885ed698d6d2d32fdd
Chalo1996 <emusyoka759@gmail.com>
2023-02-06 18:23:49 +0300

    Calls another coroutine n number of times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines."""
 
+import asyncio
 
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-delaysLst: list = []
+delaysLst: list = list()
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
