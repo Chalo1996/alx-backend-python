a05ac5824d8efbe4f136a2c51bf9fbf5b1200d80
Chalo1996 <emusyoka759@gmail.com>
2023-02-06 15:25:49 +0300

    creates a coroutine to return random time

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0-basic_async_syntax."""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    wait_random: create a coroutine that returns a random time(float)\
+        in seconds.
+
+    Args:
+        max_delay (int, optional): The arg. Defaults to 10.
+
+    Returns:
+        float: random time for which the coroutine will be executed.
+    """
+    delay: float = random.uniform(0, max_delay + 1)
+    await asyncio.sleep(delay)
+    return delay
