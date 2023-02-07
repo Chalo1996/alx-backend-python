bd95b169ce9db38ff3d9e153b15a91c344056a52
Chalo1996 <emusyoka759@gmail.com>
2023-02-07 09:04:58 +0300

    2-measure_runtime.py runner script

--- 0x01-python_async_function/2-main.py
+++ 0x01-python_async_function/2-main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""2-main."""
 
+import asyncio
 measure_time = __import__('2-measure_runtime').measure_time
 
-n = 5
-max_delay = 9
+n: int = 5
+max_delay: int = 9
 
-print(measure_time(n, max_delay))
+
+async def get_res() -> None:
+    """Call measure_time asychronously."""
+    print(await measure_time(n, max_delay))
+
+asyncio.run(get_res())
