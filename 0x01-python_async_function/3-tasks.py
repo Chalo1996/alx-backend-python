40f0e9a61c8bbf55a225a002f3eecf5e5778821d
Chalo1996 <emusyoka759@gmail.com>
2023-02-07 09:55:10 +0300

    returns a coroutine task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,12 @@
 """3-tasks."""
 
 import asyncio
+from typing import Callable
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> object:
+def task_wait_random(max_delay: int) -> Callable[[None], float]:
     """
     task_wait_random: creates a coroutine task.
 
@@ -16,4 +17,8 @@ def task_wait_random(max_delay: int) -> object:
     Returns:
         task: a coroutine task
     """
-    return (asyncio.create_task(wait_random(max_delay)))
+
+    def innerFunc():
+        return asyncio.create_task(wait_random(max_delay))
+
+    return innerFunc()
