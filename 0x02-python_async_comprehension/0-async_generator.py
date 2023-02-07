28b6260d47584d970440622594415f2e78ead238
Chalo1996 <emusyoka759@gmail.com>
2023-02-07 17:40:14 +0300

    Generates an async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""0-async_generator."""
+
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    """
+    async_generator: Generates a generator of integers.
+
+    Yields:
+        Generator[int, None, None]: A random number through every loop.
+    """
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
