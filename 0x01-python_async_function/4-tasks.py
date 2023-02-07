6f52b47b096a3a15374fd594065e2c6a96654a1c
Chalo1996 <emusyoka759@gmail.com>
2023-02-07 10:37:37 +0300

    returns a coroutine task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines."""
 
-import asyncio
 
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
